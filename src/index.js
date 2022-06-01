d66b9b5de8388d8cd29a55ea99243904e5f316b6
kblatsos <kblatsos@infili.com>
2022-06-01 12:51:59 +0300

    react

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+serviceWorkerRegistration.register();
---
+++ src/pages/Welcome.js
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Link to="new-user">New User</Link>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Welcome;
